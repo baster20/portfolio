1155cc2c8d129ecefde38f8fafbf2bfd5f324f0d
Oleg Demchenko <ltvxtyrjj@gmail.com>
2019-01-18 22:52:17 +0200

    Fix(main): Fix FORWARD_PROTO use if isset.

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -9,7 +9,7 @@
 /**
  * Handle SSL reverse proxy
  */
-if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+if (isset ($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
     $_SERVER['HTTPS']='on';
 
 if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
